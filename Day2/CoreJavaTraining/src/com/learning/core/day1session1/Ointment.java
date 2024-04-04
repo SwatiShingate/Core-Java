ecea0ca6e4313082faf2ff69327c5237bd11e98b
SwatiShingate <97164247+SwatiShingate@users.noreply.github.com>
2024-04-04 23:27:25 +0530

    Add files via upload

---
+++ Day2/CoreJavaTraining/src/com/learning/core/day1session1/Ointment.java
@@ -0,0 +1,12 @@
+package com.learning.core.day1session1;
+
+public class Ointment implements MedicineInfo
+{
+		public void displayLable()
+		{
+			System.out.println("Ointment is for externaluse only");
+		}
+		
+}
+	
+
